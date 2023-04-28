e5af45877ea742c5f4d4b27f5f6700ea9a4050da
Gerardo Gozain <gozain@gmail.com>
2023-04-28 11:35:14 +0100

    added autoscaler manifest

--- main_eks.tf
+++ main_eks.tf
@@ -319,31 +319,31 @@ resource "kubectl_manifest" "karpenter_example_deployment" {
 
 ############# LOAD BALANCER #######################
 
-resource "helm_release" "aws_load_balancer_controller" {
-  name = "aws-load-balancer-controller"
+# resource "helm_release" "aws_load_balancer_controller" {
+#   name = "aws-load-balancer-controller"
 
-  repository = "https://aws.github.io/eks-charts"
-  chart      = "aws-load-balancer-controller"
-  namespace  = "kube-system"
-  version    = "1.4.4"
+#   repository = "https://aws.github.io/eks-charts"
+#   chart      = "aws-load-balancer-controller"
+#   namespace  = "kube-system"
+#   version    = "1.4.4"
 
-  set {
-    name  = "replicaCount"
-    value = 1
-  }
+#   set {
+#     name  = "replicaCount"
+#     value = 1
+#   }
 
-  set {
-    name  = "clusterName"
-    value = module.eks.cluster_id
-  }
+#   set {
+#     name  = "clusterName"
+#     value = module.eks.cluster_id
+#   }
 
-  set {
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
+#   set {
+#     name  = "serviceAccount.name"
+#     value = "aws-load-balancer-controller"
+#   }
 
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = module.aws_load_balancer_controller_irsa_role.iam_role_arn
-  }
-}
+#   set {
+#     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+#     value = module.aws_load_balancer_controller_irsa_role.iam_role_arn
+#   }
+# }
