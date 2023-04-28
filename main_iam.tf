952f9722eded7be22313c07eb229c95f10d49eb5
Gerardo Gozain <gozain@gmail.com>
2023-04-28 11:37:59 +0100

    removed load balancer

--- main_iam.tf
+++ main_iam.tf
@@ -123,18 +123,18 @@ module "cluster_autoscaler_irsa_role" {
 
 /// Load Balancer Controler Role
 
-module "aws_load_balancer_controller_irsa_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "5.3.1"
+# module "aws_load_balancer_controller_irsa_role" {
+#   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+#   version = "5.3.1"
 
-  role_name = "aws-load-balancer-controller"
+#   role_name = "aws-load-balancer-controller"
 
-  attach_load_balancer_controller_policy = true
+#   attach_load_balancer_controller_policy = true
 
-  oidc_providers = {
-    ex = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
-    }
-  }
-}
+#   oidc_providers = {
+#     ex = {
+#       provider_arn               = module.eks.oidc_provider_arn
+#       namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+#     }
+#   }
+# }
