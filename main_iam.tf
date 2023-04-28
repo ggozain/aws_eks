e169692ad6911b0c60c9c2c6156d42e784e1b851
Gerardo Gozain <gozain@gmail.com>
2023-04-28 10:54:01 +0100

    load balancer back

--- main_iam.tf
+++ main_iam.tf
@@ -50,15 +50,13 @@ module "iam-assumable-role-with-oidc" {
   provider_url = var.tfc_hostname
 
   role_policy_arns = [
-    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",    
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
   ]
 
   oidc_fully_qualified_audiences = ["aws.workload.identity", "sts.amazonaws.com"]
   oidc_subjects_with_wildcards   = ["organization:gozain-lab:*"]
 
 }
-
-
 
 module "user1_iam_user" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-user"
@@ -106,21 +104,21 @@ module "eks_admins_iam_group" {
 
 /// Autoscaler IAM role
 
-# module "cluster_autoscaler_irsa_role" {
-#   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-#   version = "5.3.1"
+module "cluster_autoscaler_irsa_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.3.1"
 
-#   role_name                        = "cluster-autoscaler"
-#   attach_cluster_autoscaler_policy = true
-#   cluster_autoscaler_cluster_ids   = [module.eks.cluster_id]
+  role_name                        = "cluster-autoscaler"
+  attach_cluster_autoscaler_policy = true
+  cluster_autoscaler_cluster_ids   = [module.eks.cluster_id]
 
-#   oidc_providers = {
-#     ex = {
-#       provider_arn               = module.eks.oidc_provider_arn
-#       namespace_service_accounts = ["kube-system:cluster-autoscaler"]
-#     }
-#   }
-# }
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:cluster-autoscaler"]
+    }
+  }
+}
 
 
 /// Load Balancer Controler Role
