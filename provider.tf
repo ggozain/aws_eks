cafe80c8fe58aaf287c8a920e660e17fee6c6fcb
Gerardo Gozain <gozain@gmail.com>
2023-05-17 14:10:58 +0100

    changed helm provider

--- provider.tf
+++ provider.tf
@@ -47,20 +47,20 @@ provider "kubernetes" {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
     # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
+    args = ["eks", "get-token", "--cluster-name", var.eks_cluster_name]
   }
 }
 
 provider "helm" {
   kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    host                   = data.aws_eks_cluster.default.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
 
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
       # This requires the awscli to be installed locally where Terraform is executed
-      args = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
+      args = ["eks", "get-token", "--cluster-name", var.eks_cluster_name]
     }
   }
 }
@@ -75,6 +75,6 @@ provider "kubectl" {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
     # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
+    args = ["eks", "get-token", "--cluster-name", var.eks_cluster_name]
   }
 }
