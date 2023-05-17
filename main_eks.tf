2bd760a923f6639201ae024bea9626fef2980dab
Gerardo Gozain <gozain@gmail.com>
2023-05-17 15:24:37 +0100

    2

--- main_eks.tf
+++ main_eks.tf
@@ -337,6 +337,7 @@ resource "helm_release" "aws_load_balancer_controller" {
   set {
     name  = "clusterName"
     value = data.aws_eks_cluster.default.cluster_id
+  }
 
   set {
     name  = "serviceAccount.name"
@@ -347,5 +348,4 @@ resource "helm_release" "aws_load_balancer_controller" {
     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
     value = module.aws_load_balancer_controller_irsa_role.iam_role_arn
   }
-}
 }
