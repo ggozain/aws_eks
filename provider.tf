25bef4520ab67f63f7eaa0696a342dd27a7231bb
Gerardo Gozain <gozain@gmail.com>
2023-05-17 14:12:28 +0100

    changed helm provider

--- provider.tf
+++ provider.tf
@@ -54,6 +54,7 @@ provider "kubernetes" {
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.default.endpoint
+    token                  = data.aws_eks_cluster_auth.default.token
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
 
     exec {
