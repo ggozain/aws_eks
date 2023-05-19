19297f2fb6ff1e0a68a8a7f837bb774061060fe0
Gerardo Gozain <gozain@gmail.com>
2023-05-19 14:11:56 +0100

    added load balancer

--- main_eks.tf
+++ main_eks.tf
@@ -321,31 +321,31 @@ module "eks" {
 
 ############# LOAD BALANCER #######################
 
-# resource "helm_release" "aws_load_balancer_controller" {
-#   name = "aws-load-balancer-controller"
+resource "helm_release" "aws_load_balancer_controller" {
+  name = "aws-load-balancer-controller"
 
-#   repository = "https://aws.github.io/eks-charts"
-#   chart      = "aws-load-balancer-controller"
-#   namespace  = "kube-system"
-#   version    = "1.4.4"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.4.4"
 
-#   set {
-#     name  = "replicaCount"
-#     value = 1
-#   }
+  set {
+    name  = "replicaCount"
+    value = 1
+  }
 
-#   set {
-#     name  = "clusterName"
-#     value = data.aws_eks_cluster.default.id
-#   }
+  set {
+    name  = "clusterName"
+    value = data.aws_eks_cluster.default.id
+  }
 
-#   set {
-#     name  = "serviceAccount.name"
-#     value = "aws-load-balancer-controller"
-#   }
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
 
-#   set {
-#     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-#     value = module.aws_load_balancer_controller_irsa_role.iam_role_arn
-#   }
-# }
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.aws_load_balancer_controller_irsa_role.iam_role_arn
+  }
+}
